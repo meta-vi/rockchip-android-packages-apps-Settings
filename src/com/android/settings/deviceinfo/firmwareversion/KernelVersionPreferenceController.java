42c9db438daad8006f806af6d75efbc3e80c49b2
Jian Qiu <qiujian@rock-chips.com>
2020-03-04 17:22:23 +0800

    show attationkey in kernel version
    Signed-off-by: Jian Qiu <qiujian@rock-chips.com>
    Change-Id: I5ca29b7d89ada50f1ec57ba863f81cd7582393a7

--- src/com/android/settings/deviceinfo/firmwareversion/KernelVersionPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/KernelVersionPreferenceController.java
@@ -17,11 +17,27 @@
 package com.android.settings.deviceinfo.firmwareversion;
 
 import android.content.Context;
+import android.os.SystemClock;
+import android.text.TextUtils;
+import android.widget.Toast;
+import android.util.Log;
 
+import androidx.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.Utils;
 import com.android.settings.core.BasePreferenceController;
 import com.android.settingslib.DeviceInfoUtils;
 
+
 public class KernelVersionPreferenceController extends BasePreferenceController {
+
+    private static final String TAG = "KernelVersionCtrl";
+
+    private static final int DELAY_TIMER_MILLIS = 500;
+    private static final int ACTIVITY_TRIGGER_COUNT = 3;
+
+    private final long[] mHits = new long[ACTIVITY_TRIGGER_COUNT];
 
     public KernelVersionPreferenceController(Context context, String preferenceKey) {
         super(context, preferenceKey);
@@ -35,5 +51,39 @@ public class KernelVersionPreferenceController extends BasePreferenceController 
     @Override
     public CharSequence getSummary() {
         return DeviceInfoUtils.getFormattedKernelVersion(mContext);
+    }
+    @Override
+    public boolean handlePreferenceTreeClick(Preference preference) {
+
+        Log.e(TAG, "handlePreferenceTreeClick");
+        if (!TextUtils.equals(preference.getKey(), getPreferenceKey())) {
+            return false;
+        }
+        Log.e(TAG, "handlePreferenceTreeClick2");
+        if (Utils.isMonkeyRunning()) {
+            return false;
+        }
+        arrayCopy();
+        Log.e(TAG, "handlePreferenceTreeClick3");
+        mHits[mHits.length - 1] = SystemClock.uptimeMillis();
+
+        if (mHits[0] >= (SystemClock.uptimeMillis() - DELAY_TIMER_MILLIS)) {
+            try {
+                String value = android.os.SystemProperties.get("ro.boot.deviceid", "");
+                if (TextUtils.isEmpty(value) || "none".equals(value)) {
+                    Toast.makeText(mContext, R.string.attestation_device_id_failed, Toast.LENGTH_SHORT).show();
+                } else if ("success".equals(value)) {
+                    Toast.makeText(mContext, R.string.attestation_device_id_success, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(mContext, value, Toast.LENGTH_SHORT).show();
+                }
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return  true;
+    }
+    void arrayCopy() {
+        System.arraycopy(mHits, 1, mHits, 0, mHits.length - 1);
     }
 }
