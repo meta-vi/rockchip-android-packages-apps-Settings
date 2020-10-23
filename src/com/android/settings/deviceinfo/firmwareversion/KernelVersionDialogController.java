4899ff5e9261014024ed514303464e5c25835c14
gary_gen <Gary_Gen@asus.com>
2020-10-23 15:45:38 +0800

    AsusDebugger: select Kernel version preference in about tablet to open AsusDebugger
    Click 10 times on Kernel version preference to start activity.
    
    Change-Id: If6ab32646f074d16851cc4506d9e2ccbb15748d6

--- src/com/android/settings/deviceinfo/firmwareversion/KernelVersionDialogController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/KernelVersionDialogController.java
@@ -18,10 +18,30 @@ package com.android.settings.deviceinfo.firmwareversion;
 
 import android.support.annotation.VisibleForTesting;
 
+import android.util.Log;
+import android.view.View;
 import com.android.settings.R;
 import com.android.settingslib.DeviceInfoUtils;
+import android.content.Context;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.SystemClock;
+import android.widget.Toast;
+import android.text.TextUtils;
 
-public class KernelVersionDialogController {
+public class KernelVersionDialogController implements View.OnClickListener {
+
+    private static final String TAG = "KernelVersionDialogController";
+    private static final int DELAY_TIMER_MILLIS = 500;
+    private static final int ACTIVITY_TRIGGER_COUNT = 2;
+    private static final int TAPS_TO_START_DEBUGGER = 10;
+    private int mDeubggerHitCountdown;
+    private Toast mDeubggerDevHitToast;
+
+    static final String ACTION_DEBUGGER = "com.asus.debugger.SETTING_DEBUGGER";
+
+    private final Context mContext;
+    private final long[] mHits = new long[ACTIVITY_TRIGGER_COUNT];
 
     @VisibleForTesting
     static int KERNEL_VERSION_VALUE_ID = R.id.kernel_version_value;
@@ -30,13 +50,58 @@ public class KernelVersionDialogController {
 
     public KernelVersionDialogController(FirmwareVersionDialogFragment dialog) {
         mDialog = dialog;
+        mContext = dialog.getContext();
+    }
+
+    @Override
+    public void onClick(View v) {
+        arrayCopy();
+        mHits[mHits.length - 1] = SystemClock.uptimeMillis();
+        if (mHits[0] >= (SystemClock.uptimeMillis() - DELAY_TIMER_MILLIS)) {
+            mDeubggerHitCountdown--;
+        } else {
+            mDeubggerHitCountdown = TAPS_TO_START_DEBUGGER;
+            mDeubggerDevHitToast = null;
+        }
+
+        if (mDeubggerHitCountdown == 0) {
+            try {
+                Intent intent = new Intent(ACTION_DEBUGGER);
+                mContext.startActivity(intent);
+            } catch (ActivityNotFoundException e) {
+                e.printStackTrace();
+            }
+            if (mDeubggerDevHitToast != null) {
+                mDeubggerDevHitToast.cancel();
+            }
+            mDeubggerHitCountdown = TAPS_TO_START_DEBUGGER;
+        } else if (mDeubggerHitCountdown > 0
+                && mDeubggerHitCountdown < (TAPS_TO_START_DEBUGGER-2)) {
+            if (mDeubggerDevHitToast != null) {
+                mDeubggerDevHitToast.cancel();
+            }
+            mDeubggerDevHitToast = Toast.makeText(mContext, "Remaining " + mDeubggerHitCountdown,Toast.LENGTH_SHORT);
+            mDeubggerDevHitToast.show();
+        }
     }
 
     /**
      * Updates kernel version to the dialog.
      */
     public void initialize() {
+        registerClickListeners();
         mDialog.setText(KERNEL_VERSION_VALUE_ID,
                 DeviceInfoUtils.getFormattedKernelVersion(mDialog.getContext()));
+        mDeubggerHitCountdown = TAPS_TO_START_DEBUGGER;
+        mDeubggerDevHitToast = null;
+    }
+
+    private void registerClickListeners() {
+        mDialog.registerClickListener(KERNEL_VERSION_VALUE_ID, this /* listener */);
+    }
+
+    @VisibleForTesting
+    void arrayCopy() {
+        System.arraycopy(mHits, 1, mHits, 0, mHits.length - 1);
     }
 }
